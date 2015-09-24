a3bfd15c5e72d970dca35a5220299488e02be49a
donwibier <don@anykey.nl>
2015-09-24 16:15:44 +0200

    Initial Skeleton checkin

---
+++ DXRDWForDevsSample/DXRDWForDevsSample/Scripts/Site.tools.js
@@ -0,0 +1,63 @@
+﻿(function ($) {
+	 "use strict";
+
+	 var AppModule = {
+	     hamburger: {
+	         Visible: function (side) {
+	             return ($("body").hasClass("show-" + side + "-hamburger-menu"));
+	         },
+	         Show: function (side) {
+	             if (!this.Visible(side)) {
+	                 ($("body").addClass("show-" + side + "-hamburger-menu"));
+	             }
+	         },
+	         Hide: function (side) {
+	             if (this.Visible(side)) {
+	                 ($("body").removeClass("show-" + side + "-hamburger-menu"));
+	             }
+	         },
+	         Toggle: function (side) {
+	             if (!this.Visible(side)) {
+	                 this.Hide(side == "left" ? "right" : "left");
+	                 this.Show(side);
+	             }
+	             else {
+	                 this.Hide(side);
+	             }
+	         }
+	     }, // hamburger
+	     realign: function () {
+             //hide hamburger menu
+	         if ($(document).width() > 549) {
+	             AppModule.hamburger.Hide("left");
+	             AppModule.hamburger.Hide("right");
+	         }
+	         if (typeof HomeBanner !== 'undefined') {	             
+	             HomeBanner.SetHeight(Math.floor(350 * $('#bannerContainer').width() / 960));
+	         }
+	     }
+	 }
+
+	 //initialization
+	 window.App = AppModule;
+	 $("*[data-navigate-action]").each(function () {
+	     if ($(this).attr("data-navigate-action") === "toggle-hamburger-left") {
+	         $(this).click(function () {
+	             AppModule.hamburger.Toggle("left");
+	         });
+	     }
+	     else if ($(this).attr("data-navigate-action") === "toggle-hamburger-right") {
+	         $(this).click(function () {
+	             AppModule.hamburger.Toggle("right");
+	         });
+	     }
+	 });
+	 $(window).on('resize', function () {
+	     AppModule.realign();
+	 });
+	 $(window).on('orientationchange', function (event) {
+         // event.orientation = portrait | landscape
+	     AppModule.realign();
+	 });
+	 AppModule.realign();
+})(jQuery);
